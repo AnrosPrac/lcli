2d8fa155d02e2453f206c7a93f66888741ee0598
AnrosPrac <sidhiqa20@gmail.com>
2026-01-12 23:05:55 +0530

    first commit

--- lum.py
+++ lum.py
@@ -13,7 +13,7 @@ from nacl.signing import SigningKey
 import binascii
 
 
-VERSION = "2.3.0"
+VERSION = "1.1.0"
 BASE_URL = "https://test-termial.onrender.com" 
 RAW_URL = "https://raw.githubusercontent.com/AnrosPrac/lcli/main"
  # Ensure this is your live URL
